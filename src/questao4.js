32983415f52adbe8cc71b0c669dd18167f483c83
Luissgustavo10 <147614304+Luissgustavo10@users.noreply.github.com>
2025-11-12 14:00:16 -0300

    Update questao4.js

--- src/questao4.js
+++ src/questao4.js
@@ -1,1 +1,11 @@
-
+const endereco = {
+  rua: "Av. Paulista",
+  numero: 1000,
+  cidade: "São Paulo",
+  cep: "01310-100"
+};
+function formatarEndereco(endereco) {
+  return `${endereco.rua}, ${endereco.numero} - ${endereco.cidade} - CEP: ${endereco.cep}`;
+}
+const enderecoFormatado = formatarEndereco(endereco);
+console.log(enderecoFormatado);

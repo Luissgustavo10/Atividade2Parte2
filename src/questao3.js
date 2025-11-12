cc49dcf107308e18c85d7a664798c6ac2d9f6633
Luissgustavo10 <147614304+Luissgustavo10@users.noreply.github.com>
2025-11-12 13:59:39 -0300

    Update questao3.js

--- src/questao3.js
+++ src/questao3.js
@@ -1,1 +1,16 @@
+const produtosJSON = `{
+  "produtos": [
+    {"nome": "Mouse", "preco": 25.90},
+    {"nome": "Teclado", "preco": 89.90},
+    {"nome": "Monitor", "preco": 450.00},
+    {"nome": "Cabo USB", "preco": 15.00}
+  ]
+}`;
+function obterProdutosAcimaDe50(jsonString) {
+  const dados = JSON.parse(jsonString);
+  const produtosAcimaDe50 = dados.produtos.filter(produto => produto.preco > 50);
+  return produtosAcimaDe50.map(produto => produto.nome);
+}
+const nomesProdutosAcimaDe50 = obterProdutosAcimaDe50(produtosJSON);
+console.log(nomesProdutosAcimaDe50);
 

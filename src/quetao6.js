105473fc06417cec050677919561ffe35850b9e0
Luissgustavo10 <147614304+Luissgustavo10@users.noreply.github.com>
2025-11-12 14:02:15 -0300

    Update quetao6.js

--- src/quetao6.js
+++ src/quetao6.js
@@ -1,1 +1,10 @@
-
+function parseJSONSafe(jsonString) {
+  try {
+    return JSON.parse(jsonString);
+  } catch (erro) {
+    console.error("Erro ao tentar converter para JSON:", erro.message);
+    return null;
+  }
+}
+const jsonInvalido = '{nome: "João", "idade": 30}'; 
+console.log(parseJSONSafe(jsonInvalido));

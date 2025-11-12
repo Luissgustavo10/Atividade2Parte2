17d83851206a623daf6a7b29f8982eb081ee46b8
Luissgustavo10 <147614304+Luissgustavo10@users.noreply.github.com>
2025-11-12 13:58:36 -0300

    Update questao2.js

--- src/questao2.js
+++ src/questao2.js
@@ -1,1 +1,11 @@
-
+const jsonString = `{
+  "usuarios": [
+    {"nome": "Carlos", "email": "carlos@email.com"},
+    {"nome": "Maria", "email": "maria@email.com"},
+    {"nome": "João", "email": "joao@email.com"}
+  ]
+}`;
+const dados = JSON.parse(jsonString);
+const emails = dados.usuarios.map(u => u.email);
+const emailsUnidos = emails.join(", ");
+console.log(emailsConcatenados);
